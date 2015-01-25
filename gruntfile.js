d9275068051a691890fcaa3144333cce742801c3
aleksey-bykov <aleksey.bykov@gmail.com>
2015-01-25 14:28:00 -0500

    Task for deploying to the gh-pages branch.

--- gruntfile.js
+++ gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
             }
         },
         buildGhPages: {
-            ghPages: { /* all default */ }
+            ghPages: { dist: 'built' }
         }
     });
 
