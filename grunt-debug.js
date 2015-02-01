25d9eddbf003dab32a7fcfadfca9d750270d5204
aleksey-bykov <aleksey.bykov@gmail.com>
2015-02-01 05:05:21 -0500

    Design changes.

--- grunt-debug.js
+++ grunt-debug.js
@@ -3,48 +3,48 @@ var grunt = require('grunt');
 var destFolder = '../gh-pages.js';
 
 grunt.initConfig({
-    'pull': {
-        'for-gh-pages': {
-            options: {
-                dest: destFolder,
-                branch: 'gh-pages'
-            }
-        },
-        'for-master': {
-            options: {
-                dest: './',
-                branch: 'master'
-            }
-        }
+    'deploy': {
+        'gh-pages': {
+        options: {
+            dest: destFolder,
+            branch: 'gh-pages'
+        }}
     }
 });
 
-grunt.registerMultiTask(
-    'pull', function () {
-        debugger;
-        var run = runOver(grunt);
+grunt.registerMultiTask('deploy', function () {
+    
+    debugger;
 
-        var options = this.options({});
-        var dest = toParameter(options, 'dest', 'a root folder to pull to');
-        var branch = toParameter(options, 'branch', 'a name of the branch to be pulled');
-
-        var done = this.async();
-        grunt.verbose.writeln('Fetching.');
-        run('git fetch', function (result) {
+    var run = runOver(grunt);
+    var options = this.options({});
+    var dest = toParameter(options, 'dest', 'a root folder to clone or pull to');
+    var branch = toParameter(options, 'branch', 'a name of the branch to be cloned or pulled');
+console.log(1);
+    var done = this.async();
+console.log(2);
+    grunt.verbose.writeln('Checking out: ' + branch);
+console.log(3);
+    run('git checkout ' + branch, function (result) {
+console.log(4);
+        grunt.verbose.writeln('Staging.');
+        run('git add -all', function (result) {
             grunt.verbose.writeln(String(result));
-            grunt.verbose.writeln('Checking out: ' + branch);
-            run('git checkout ' + branch, function (result) {
+            grunt.verbose.writeln('Commiting.');
+            run('git commit -a -m', function (result) {
                 grunt.verbose.writeln(String(result));
-                run('git pull --rebase', function (result) {
+                grunt.verbose.writeln('Pushing.');
+                run('git push', function (result) {
                     grunt.log.writeln(String(result));
                     done();
                 }, dest);
             }, dest);
         }, dest);
-    }
-);
+    }, dest);
+});
 
-grunt.tasks(['pull:for-master']);
+
+grunt.tasks(['deploy:gh-pages']);
 
 function toParameter(options, name, description) {
     var result = options[name];
