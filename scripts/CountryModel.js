ca47986dbbab54ce40755fb319b221d55a7acb26
Aleksey Bykov <abykov@eastbanctech.com>
2015-02-07 16:25:45 -0500

    "Autogenerated on Sat Feb 07 2015 16:25:45 GMT-0500 (Eastern Standard Time)"

--- scripts/CountryModel.js
+++ scripts/CountryModel.js
@@ -6,7 +6,6 @@ var models;
             this.source = ko.observable(0);
             this.noData = 'No data';
             this.countries = ko.observableArray([]);
-            this.isSourcesVisible = ko.observable(true);
             this.summaryData = ko.observable(models.CountryData.rows["ALB"]);
             this.name = ko.observable('');
             this.cats = ['Number of Enterprises', 'Density', 'Employment', 'Vallue added', 'Number of Employees', 'Size Breakdown', 'Manufacturing', 'Trade', 'Agri/Other', 'Services'];
@@ -91,9 +90,6 @@ var models;
                         return me.noData;
                     }
                 }
-            };
-            me.showSources = function () {
-                me.isSourcesVisible(!me.isSourcesVisible());
             };
             me.showSummary = function (obj, ev) {
                 var d = models.CountryData.rows[obj.code];
